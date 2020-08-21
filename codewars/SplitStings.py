f10457ccefc7ddd32fea3fea05b97e1e2e09002e
applego <applegorillappa@gmail.com>
2020-08-21 11:19:32 +0900

    codewar SplitStrings.py

---
+++ codewars/SplitStings.py
@@ -0,0 +1,30 @@
+"""
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+solution('abc') # should return ['ab', 'c_']
+solution('abcdef') # should return ['ab', 'cd', 'ef']
+"""
+
+def solution(s):
+    len_s = len(s)
+    is_odd = len_s % 2 != 0
+    arr_cnt = int((len_s+1)/2 if is_odd else len_s/2)
+    arr = []
+
+    for i in range(arr_cnt):
+#         print('i:'+str(i))
+        if i ==arr_cnt-1 and is_odd:
+            arr.append(s[i*2]+'_')
+#             print(s[i*2]+'_')
+        else:
+            arr.append(s[i*2:i*2+2])
+#             print(s[i*2:i*2+2])
+    return arr
+
+if __name__ == '__main__':
+    arg = 'abc'
+    print(solution(arg))
+    arg = 'abcdef'
+    print(solution(arg))
