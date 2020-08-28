a5953d82eb0e4ab28f0f47826ae51c9471ce2de5
applego <applegorillappa@gmail.com>
2020-08-28 12:46:10 +0900

    Valid Parentheses.py 完答

--- codewars/Valid Parentheses.py
+++ codewars/Valid Parentheses.py
@@ -4,14 +4,31 @@
 "("               =>  false
 "(())((()())())"  =>  true
 
+
+1)空文字True
+2)　)が(より左にある　False
+3)　（と）の数が合わない
 """
 
 def valid_parentheses(string):
-    return False
+    if len(string) == 0:
+        return True
+    checknum = 0
+    for s in string:
+        if s == '(':
+            checknum += 1
+        elif s == ')':
+            checknum -= 1
+        if checknum < 0:
+            return False
+    else:
+        return True if checknum == 0 else False
 
 
+print(valid_parentheses(""))#True)
 print(valid_parentheses("  ("))#False)
 print(valid_parentheses(")test"))#False)
-print(valid_parentheses(""))#True)
 print(valid_parentheses("hi())("))#False)
-print(valid_parentheses("hi(hi)()") )#True
+print(valid_parentheses("hi(hi)()"))  #True
+
+#solutionとほぼ同じ　Yeah
