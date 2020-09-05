518cefec0cc5fbe72804e23deb0e9149a9cfa243
applego <applegorillappa@gmail.com>
2020-09-05 21:47:14 +0900

    python Falseは0 isinstance(i,bool) or i!=0

--- codewars/5 kyu Moving Zeros To The End.py
+++ codewars/5 kyu Moving Zeros To The End.py
@@ -4,17 +4,17 @@ from TestCodewar import Test
  # returns[False,1,1,2,1,3,"a",0,0]
 """
 def move_zeros(array):
-    #your code here
-    return array
+  array_not_in_0 = [i for i in array if not ((type(i) is int)  and i == 0) and not ((type(i) is float) and i == 0)]
+  return array_not_in_0 + [0] * (len(array) - len(array_not_in_0))
+
+  # str_array = list(map(str, array))
+  # return [int(i) for i in str_array if i != '0']+[0]*str_array.count('0')
 
 Test = Test()
-# test.assert_equals(move_zeros([1,2,0,1,0,1,0,3,0,1]),[ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
-# test.assert_equals2(move_zeros([1,2,0,1,0,1,0,3,0,1]),[ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
-# test.assert_equals2(move_zeros([ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ]),[ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
 
 Test.describe("Basic tests")
+Test.assert_equals(move_zeros([9, 0.0, 9, 1, 2, 1, 1, 0.0, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0]),[9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 Test.assert_equals([ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ],[ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
-
 Test.assert_equals(move_zeros([1,2,0,1,0,1,0,3,0,1]),[ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
 Test.assert_equals(move_zeros([9,0.0,0,9,1,2,0,1,0,1,0.0,3,0,1,9,0,0,0,0,9]),[9,9,1,2,1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
 Test.assert_equals(move_zeros(["a",0,0,"b","c","d",0,1,0,1,0,3,0,1,9,0,0,0,0,9]),["a","b","c","d",1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
@@ -25,4 +25,8 @@ Test.assert_equals(move_zeros(["a"]),["a"])
 Test.assert_equals(move_zeros([0,0]),[0,0])
 Test.assert_equals(move_zeros([0]),[0])
 Test.assert_equals(move_zeros([False]),[False])
-Test.assert_equals(move_zeros([]),[])
+Test.assert_equals(move_zeros([]), [])
+
+# [9, 0.0, 9, 1, 2, 1, 1, 0.0, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0]
+# should equal
+# [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
