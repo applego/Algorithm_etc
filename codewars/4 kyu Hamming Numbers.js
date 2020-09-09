38182655344fb6dac5c3e6ce7fda7027b13e2d64
applego <applegorillappa@gmail.com>
2020-09-09 12:56:21 +0900

    jsのgood solution追加

--- codewars/4 kyu Hamming Numbers.js
+++ codewars/4 kyu Hamming Numbers.js
@@ -44,3 +44,17 @@ test.expect(hamming(17) == 27, "hamming(17) should be 27");
 test.expect(hamming(18) == 30, "hamming(18) should be 30");
 test.expect(hamming(19) == 32, "hamming(19) should be 32");
 test.expect(hamming(145) == 5120, "hamming(145) should be 5120");
+
+//solutions
+function hamming (n) {
+    var seq = [1];
+    var i2 = 0, i3 = 0, i5 = 0;
+    for (var i = 1; i < n; i++) {
+      var x = Math.min(2 * seq[i2], 3 * seq[i3], 5 * seq[i5]);
+      seq.push(x);
+      if (2 * seq[i2] <= x) i2++;
+      if (3 * seq[i3] <= x) i3++;
+      if (5 * seq[i5] <= x) i5++;
+    }
+    return seq[n-1];
+  }
