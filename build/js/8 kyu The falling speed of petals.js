2435fb728ff576dfeb34745f0a8584f708454da0
applego <applegorillappa@gmail.com>
2020-09-17 12:29:13 +0900

    Typescriptデバッグ実行成功

---
+++ build/js/8 kyu The falling speed of petals.js
@@ -0,0 +1,11 @@
+"use strict";
+// export function sakuraFall(v: number): number {
+//     console.log("sakura");
+//     return 0;
+// }
+// sakuraFall(5);
+// sakuraFall(10);
+// sakuraFall(-1);
+// sakuraFall(0);
+console.log("sakuraFall");
+//# sourceMappingURL=8 kyu The falling speed of petals.js.map
---
+++ codewars/8 kyu The falling speed of petals.ts
@@ -0,0 +1,11 @@
+// export function sakuraFall(v: number): number {
+//     console.log("sakura");
+//     return 0;
+// }
+
+// sakuraFall(5);
+// sakuraFall(10);
+// sakuraFall(-1);
+// sakuraFall(0);
+
+console.log("sakuraFall");
