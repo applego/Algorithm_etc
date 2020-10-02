501ee8b12e63ac232450ae811dff8060c17a847f
applego <applegorillappa@gmail.com>
2020-10-02 22:32:42 +0900

    一応解けた。。再起関数、場合分け、うまいパターン網羅 の方法を見つけたら効率的にとけるのかな

--- codewars/5 kyu Write out numbers.py
+++ codewars/5 kyu Write out numbers.py
@@ -4,6 +4,7 @@
 10の位
 　
 """
+# import itertools
 dic_num_word = {n: w for n, w in enumerate('zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split())}
 dic_num_word.update({n * 10: w for n, w in enumerate('twenty thirty forty fifty sixty seventy eighty ninety hundred'.split(), 2)})
 
@@ -16,10 +17,10 @@ def parseOne(n):
         return dic_num_word[n]
 
 def getOverHundred(h):
-    ret = ''
-    ret += parseOne(h)  # dic_num_word[h]
-    if len(ret) > 0:
-        ret += ' hundred'
+    ret = []
+    ret.append(parseOne(h))  # dic_num_word[h]
+    if len(ret[0]) > 0:
+        ret.append('hundred')
     return ret
 
 def getUnderHunderd(n):
@@ -31,6 +32,8 @@ def getUnderHunderd(n):
             return ''
         if n[0] == '0':
             return parseOne(int(n[1]))
+        if int(n) in dic_num_word.keys():
+            return dic_num_word[int(n)]
         key = int(n[0]) * 10
         ret += parseOne(key)  # dic_num_word[key]
         one = parseOne(int(n[1]))
@@ -41,28 +44,35 @@ def getUnderHunderd(n):
 def parseOverThousandGroup(group):
     if group == '100':
         return 'one thousand'
-    str_num = parseUnderThousandGroup(group)
-    str_num += ' thousand'
-    return str_num
+    ret = parseUnderThousandGroup(group)
+    if isinstance(ret, str):
+        return ret + ' thousand'
+    else:
+        ret.append('thousand')
+        return ret
 
 def parseUnderThousandGroup(group):
-    str_num = ''
+    # ret = []
     len_group = len(group)
     if len_group > 2:
-        str_num += getOverHundred(group[0])
-        one = getUnderHunderd(group[1:])
-        if len(one) > 0:
-            str_num += one
+        # ret.append(getOverHundred(group[0]))
+        ret = getOverHundred(group[0])
+        if len(ret[0]) > 0:
+            ret.append(getUnderHunderd(group[1:]))
+        else:
+            ret = getUnderHunderd(group[1:])
+        # ret.append(getUnderHunderd(group[1:]))
     else:
-        str_num += getUnderHunderd(group)
-    return str_num
-
+        ret = getUnderHunderd(group)
+    return ret
 
 def number2words(n):
     """ works for numbers between 0 and 999999 """
     print(n)
+    #100
     if n == 100:
         return 'one hundred'
+    # 辞書にあれば
     if n in dic_num_word.keys():
         return dic_num_word[n]
 
@@ -70,15 +80,29 @@ def number2words(n):
     str_r = ''.join(list(reversed(str_n)))
     splited_3_number = [str_r[i:i + 3][::-1] for i in range(0, len(str_n), 3)][::-1]
 
-    ans = ''
+    arr_ans = []
     if len(splited_3_number) > 1:
-        ans += parseOverThousandGroup(splited_3_number[0])
-        tmp = parseUnderThousandGroup(splited_3_number[1])
-        if len(tmp) > 0:
-            ans += ' ' + tmp
+        tmp1 = parseOverThousandGroup(splited_3_number[0])
+        if isinstance(tmp1, str):
+            arr_ans.append(tmp1)
+        else:
+            arr_ans = tmp1
+        tmp2 = parseUnderThousandGroup(splited_3_number[1])
+        if isinstance(tmp2, str):
+            arr_ans.append(tmp2)
+        else:
+            for i in tmp2:
+                arr_ans.append(i)
+        # arr_ans.append(parseUnderThousandGroup(splited_3_number[1]))
     else:
-        ans += parseUnderThousandGroup(splited_3_number[0])
-    return ans.strip()
+        arr_ans = parseUnderThousandGroup(splited_3_number[0])
+    # return ' '.join([group for group in list(itertools.chain.from_iterable(arr_ans)) if len(group) > 0])
+
+    if isinstance(arr_ans, str):
+        return arr_ans
+    else:
+        return ' '.join([group for group in arr_ans if len(group) > 0])
+
 
 from TestCodewar import Test
 test = Test()
@@ -94,15 +118,58 @@ test = Test()
 # test.assert_equals(number2words(98), "ninety-eight")
 # test.assert_equals(number2words(100), "one hundred")
 # test.assert_equals(number2words(301), "three hundred one")
-test.assert_equals(number2words(793), "seven hundred ninety-three")
-# 'three hundredone' should equal
-# 'three hundred one'
+# test.assert_equals(number2words(793), "seven hundred ninety-three")
+
 # test.assert_equals(number2words(800), "eight hundred")
 # test.assert_equals(number2words(650), "six hundred fifty")
 # test.assert_equals(number2words(1000), "one thousand")
 # test.assert_equals(number2words(1003), "one thousand three")
+# test.assert_equals(number2words(3052), 'three thousand fifty-two')
+# test.assert_equals(number2words(7300), 'seven thousand three hundred')
+# test.assert_equals(number2words(7217), 'seven thousand two hundred seventeen')
+# test.assert_equals(number2words(888887), 'seven thousand two hundred seventeen') #答え違う
+
+# 'seven thousand two hundred ten-seven' should equal
+# 'seven thousand two hundred seventeen'
+
+# 'three hundredone' should equal
+# 'three hundred one'
 
 # 'three hundred five thousand two' should equal
 # 'three thousand fifty-two'
 
-test.assert_equals(number2words(3052), 'three thousand fifty-two')
+#長すぎやし　いきあたりばったりすぎるな
+#BP
+
+words = "zero one two three four five six seven eight nine" + \
+" ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty" + \
+" thirty forty fifty sixty seventy eighty ninety"
+words = words.split(' ')
+
+def number2words_(n):
+    if n < 20:
+        print(words[n])
+        return words[n]
+    elif n < 100:
+        print(words[18 + n // 10] + ('' if n % 10 == 0 else '-' + words[n % 10]))
+        return words[18 + n // 10] + ('' if n % 10 == 0 else '-' + words[n % 10])
+    elif n < 1000:
+        print(number2words_(n // 100) + " hundred" + (' ' + number2words_(n % 100) if n % 100 > 0 else ''))
+        return number2words_(n // 100) + " hundred" + (' ' + number2words_(n % 100) if n % 100 > 0 else '')
+    elif n < 1000000:
+        print(number2words_(n // 1000) + " thousand" + (' ' + number2words_(n % 1000) if n % 1000 > 0 else ''))
+        return number2words_(n // 1000) + " thousand" + (' ' + number2words_(n % 1000) if n % 1000 > 0 else '')
+
+test.assert_equals(number2words_(1000), "one thousand")
+test.assert_equals(number2words_(1003), "one thousand three")
+test.assert_equals(number2words_(3052), 'three thousand fifty-two')
+test.assert_equals(number2words_(7300), 'seven thousand three hundred')
+test.assert_equals(number2words_(7217), 'seven thousand two hundred seventeen')
+
+#! 再起関数　場合分け
+"""
+20まで
+100まで
+1000まで
+それ以上
+"""
