a3e6fa2a8e6dc684a274cd1efaf29ac70c5d2042
applego <applegorillappa@gmail.com>
2020-10-07 00:22:54 +0900

    TypeScriptの規約の違い？でBPだとエラーになるのがある

--- build/js/codewars/6 kyu Counting Duplicates.js
+++ build/js/codewars/6 kyu Counting Duplicates.js
@@ -1,13 +1,6 @@
 "use strict";
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.duplicateCount_ = exports.duplicateCount = void 0;
+exports.duplicateCount = void 0;
 function duplicateCount(text) {
     //配列化　小文字化
     var lowercase = text.toLowerCase();
@@ -33,17 +26,17 @@ function duplicateCount(text) {
 }
 exports.duplicateCount = duplicateCount;
 //BP
-function duplicateCount_(text) {
-    var lowercaseText = text.toLocaleLowerCase();
-    var set = new Set(lowercaseText);
-    var textArray = __spreadArrays(lowercaseText);
-    return __spreadArrays(set).reduce(function (accumulator, value) {
-        var characterCount = textArray.filter(function (character) { return character === value; }).length;
-        if (characterCount > 1) {
-            return accumulator + 1;
-        }
-        return accumulator;
-    }, 0);
-}
-exports.duplicateCount_ = duplicateCount_;
+//error https://stackoverflow.com/questions/49218765/typescript-and-iterator-type-iterableiteratort-is-not-an-array-type
+// export function duplicateCount_(text: string): number{
+//   const lowercaseText = text.toLocaleLowerCase();
+//   const set = new Set(lowercaseText);
+//   const textArray = [...lowercaseText];
+//   return [...set].reduce((accumulator, value) => {
+//       const characterCount = textArray.filter((character) => character === value).length;
+//       if (characterCount > 1) {
+//           return accumulator + 1;
+//       }
+//       return accumulator
+//   }, 0);
+// }
 //# sourceMappingURL=6 kyu Counting Duplicates.js.map
