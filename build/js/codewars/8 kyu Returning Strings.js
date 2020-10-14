80a1248e86b52a7c6b3b91603a88040404283d45
applego <applegorillappa@gmail.com>
2020-10-14 23:06:40 +0900

    TypeScriptばっか F5デバッグだといけるしなぜ

---
+++ build/js/codewars/8 kyu Returning Strings.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = void 0;
+exports.greet = function (name) { return "Hello, " + name + " how are you doing today?"; };
+//# sourceMappingURL=8 kyu Returning Strings.js.map
--- codewars/6 kyu Your order, please.ts
+++ codewars/6 kyu Your order, please.ts
@@ -21,25 +21,25 @@ export function order(words: string): string {
             return splitedWords[val[0]];
         }).join(' ');
 
-    //ここまでで提出
-    const splited: string[] = words.split(' ');
-    let hash_idx_num: Hash_idx_num = {};
-    for (let i = 0; i < splited.length; i++) {
-        const num: string = splited[i].replace(/[^0-9]/g, '');
-        hash_idx_num[i] = parseInt(num); //key:配列にしたときとインデックスの初期値,value: 含まれる数字
-    }
+    // //ここまでで提出
+    // const splited: string[] = words.split(' ');
+    // let hash_idx_num: Hash_idx_num = {};
+    // for (let i = 0; i < splited.length; i++) {
+    //     const num: string = splited[i].replace(/[^0-9]/g, '');
+    //     hash_idx_num[i] = parseInt(num); //key:配列にしたときとインデックスの初期値,value: 含まれる数字
+    // }
 
-    // Create items array
-    let sortedByValue = Object.keys(hash_idx_num).map(function (key) {
-        const key_num :number = parseInt(key);
-        return [key_num, hash_idx_num[key_num]];
-    });
+    // // Create items array
+    // let sortedByValue = Object.keys(hash_idx_num).map(function (key) {
+    //     const key_num :number = parseInt(key);
+    //     return [key_num, hash_idx_num[key_num]];
+    // });
 
-    // Sort the array based on the second element
-    sortedByValue.sort(function (first, second) {
-        return first[1] - second[1];
-    });
-    return sortedByValue.map(a => splited[a[0]]).join(' ');
+    // // Sort the array based on the second element
+    // sortedByValue.sort(function (first, second) {
+    //     return first[1] - second[1];
+    // });
+    // return sortedByValue.map(a => splited[a[0]]).join(' ');
 }
 
 //BP
