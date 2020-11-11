85eb79383aade1d1aebaf243867ac1e1b760df1e
applego <applegorillappa@gmail.com>
2020-11-11 13:04:55 +0900

    rust_study https://doc.rust-jp.rs/the-rust-programming-language-ja/1.6/book/variable-bindings.html

--- rust_study/projects/hello_world/src/main.rs
+++ rust_study/projects/hello_world/src/main.rs
@@ -1,3 +1,25 @@
 fn main(){
     println!("Hello, world!");
+
+    // 変数束縛
+    let _x = 5;
+
+    // パターン
+    let (_x,_y) = (1,2);
+
+    // 型アノテーション
+    let _x: i32 = 5;
+
+    // 可変性
+
+    // エラー
+    // let x = 5;
+    // x = 10;
+
+    let mut _x = 5;  // mut x: i32
+    _x = 10;
+
+    // 束縛を初期化する
+    let _x:i32;
+    println!("Hello, world!");
 }
