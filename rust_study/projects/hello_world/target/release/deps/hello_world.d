261ba96ad620259b5ef9c7b8065dfe13705d7a25
applego <applegorillappa@gmail.com>
2020-11-11 12:53:30 +0900

    Cargo リリースビルド

---
+++ rust_study/projects/hello_world/target/release/deps/hello_world.d
@@ -0,0 +1,5 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/hello_world/target/release/deps/hello_world: src/main.rs
+
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/hello_world/target/release/deps/hello_world.d: src/main.rs
+
+src/main.rs:
---
+++ rust_study/projects/hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/hello_world/target/release/hello_world: /Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/hello_world/src/main.rs
