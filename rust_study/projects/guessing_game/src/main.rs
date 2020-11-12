7011b38cbbd9faf392378fb0809988a553b8d64e
applego <applegorillappa@gmail.com>
2020-11-13 01:37:08 +0900

    完成!! Rust好きな言語1位なのがわかるわ このチュートリアルも最高

--- rust_study/projects/guessing_game/src/main.rs
+++ rust_study/projects/guessing_game/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is {}", secret_number);
+    println!("The secret number is {}", secret_number); //todo リリース時は表示しない
 
     loop{
         println!("Please input your guess.");
