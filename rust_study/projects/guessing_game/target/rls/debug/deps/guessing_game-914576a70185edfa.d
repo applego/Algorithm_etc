83bef9a68fa8a5024d2d16a29f73d989ccdd1247
applego <applegorillappa@gmail.com>
2020-11-13 01:05:50 +0900

    add rand = "0.3.14" in Cargo.toml  run cargo build 　⇛ダウンロードしてくれる   二回目以降はしない

---
+++ rust_study/projects/guessing_game/target/rls/debug/deps/guessing_game-914576a70185edfa.d
@@ -0,0 +1,5 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/guessing_game/target/rls/debug/deps/guessing_game-914576a70185edfa.rmeta: src/main.rs
+
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/guessing_game/target/rls/debug/deps/guessing_game-914576a70185edfa.d: src/main.rs
+
+src/main.rs:
---
+++ rust_study/projects/guessing_game/target/rls/debug/deps/guessing_game-e9d4bfdef8c2a623.d
@@ -0,0 +1,5 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/guessing_game/target/rls/debug/deps/guessing_game-e9d4bfdef8c2a623.rmeta: src/main.rs
+
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/guessing_game/target/rls/debug/deps/guessing_game-e9d4bfdef8c2a623.d: src/main.rs
+
+src/main.rs:
