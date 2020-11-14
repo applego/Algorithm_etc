a7c9d4d3d925fe645bd29cb1b00c22012b2a7669
applego <applegorillappa@gmail.com>
2020-11-14 23:04:11 +0900

    rust_studyにcodewars_rustプロジェクトを追加 ⇛こっちでrustは解く

---
+++ rust_study/projects/codewars_rust/target/debug/codewars_rust.d
@@ -0,0 +1,1 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/codewars_rust/target/debug/codewars_rust: /Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/codewars_rust/src/main.rs
---
+++ rust_study/projects/codewars_rust/target/debug/deps/codewars_rust.d
@@ -0,0 +1,5 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/codewars_rust/target/debug/deps/codewars_rust: src/main.rs
+
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/codewars_rust/target/debug/deps/codewars_rust.d: src/main.rs
+
+src/main.rs:
