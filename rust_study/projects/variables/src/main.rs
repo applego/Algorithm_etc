707a4213019082b8511706e4d678b9fb0cb1beca
applego <applegorillappa@gmail.com>
2020-11-14 21:59:33 +0900

    変数章 終わり

--- rust_study/projects/variables/src/main.rs
+++ rust_study/projects/variables/src/main.rs
@@ -3,4 +3,21 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    // 変数と定数(constants)の違い
+    const MAX_POINTS: u32 = 100_000;
+
+    // シャドーイング
+    let x2 = 5;
+    let x2 = x2 + 1;
+    let x2 = x2 * 2;
+    println!("The value of x2 is: {}", x2);
+
+    let spaces = "   ";
+    println!("The value of spaces is: {}", spaces);
+    let spaces = spaces.len();
+    println!("The value of spaces is: {}", spaces);
+
+    // let mut spaces_ng = "   ";
+    // spaces_ng = spaces.len();// これはできない
 }
