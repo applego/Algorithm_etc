4e8c5c72dea422db5e11d67ab78a2fbbc9f9abcb
applego <applegorillappa@gmail.com>
2020-11-14 22:17:58 +0900

    データ型章　終わり これは、実際に稼働しているRustの安全機構の最初の例になります。低レベル言語の多くでは、 この種のチェックは行われないため、間違った添え字を与えると、無効なメモリにアクセスできてしまいます。 Rustでは、メモリアクセスを許可し、処理を継続する代わりに即座にプログラムを終了することで、 この種のエラーからプログラマを保護しています。Rustのエラー処理については、第9章でもっと議論します。

--- rust_study/projects/variables/src/main.rs
+++ rust_study/projects/variables/src/main.rs
@@ -20,4 +20,69 @@ fn main() {
 
     // let mut spaces_ng = "   ";
     // spaces_ng = spaces.len();// これはできない
+
+    // データ型
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+    //　スカラー型
+
+    // 浮動小数点型
+    let x3 = 2.0; // f64
+    let y: f32 = 3.0; // f32
+
+    // 数値演算
+    let sum = 5 + 10;
+
+    let difference = 95.5 - 4.3;
+
+    let product = 4 * 30;
+
+    let quotient = 56.7 / 32.2;
+
+    let remainder = 43 % 5;
+
+    // 論理値型
+    let t = true;
+
+    let f: bool = false; // 明示的型注釈付きで
+
+    // 複合型
+    // タプル型
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is :{}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+
+    // 配列型
+    /*
+     配列によっても、複数の値のコレクションを得ることができます。
+     タプルと異なり、配列の全要素は、 同じ型でなければなりません。
+     Rustの配列は、他の言語と異なっています。Rustの配列は、 固定長なのです:
+      一度宣言されたら、サイズを伸ばすことも縮めることもできません。
+    */
+    let a = [1, 2, 3, 4, 5];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let first = a[0];
+    let second = a[1];
+
+    let index = 10;
+    let element = a[index];
+    println!("The value of element is:{}", element);
 }
