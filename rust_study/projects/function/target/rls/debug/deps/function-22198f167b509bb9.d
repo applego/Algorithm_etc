1e11f44cfaa28664558ff71f2f183ca1ac8f2b1a
applego <applegorillappa@gmail.com>
2020-11-15 14:39:10 +0900

    add another_function() to main.rs

---
+++ rust_study/projects/function/target/rls/debug/deps/function-22198f167b509bb9.d
@@ -0,0 +1,5 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/function/target/rls/debug/deps/function-22198f167b509bb9.rmeta: src/main.rs
+
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/function/target/rls/debug/deps/function-22198f167b509bb9.d: src/main.rs
+
+src/main.rs:
