a274e46aabd8885c29fca9c952c623851de28425
applego <applegorillappa@gmail.com>
2020-11-15 14:58:39 +0900

    関数章終わり

--- rust_study/projects/function/src/main.rs
+++ rust_study/projects/function/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     //expected expression, found statement (`let`)
     // let x = (let y=6);
 
+    // 関数本体は、文と式を含む
     let x =5;
     let y = {
         let x=3;
@@ -11,9 +12,25 @@ fn main() {
     };
 
     println!("The value fo y is: {}",y);
+
+    // 戻り値のある関数
+    let x = five();
+    println!("The value fo x is: {}",x);
+
+    let x = plus_one(5);
+    println!("The value fo x is: {}",x);
 }
 
 fn another_function(x:i32,y:i32){
     println!("The value of x is:{}",x);
     println!("The value of y is:{}",y);
+}
+
+fn five() -> i32{
+    5
+}
+
+fn plus_one(x:i32)-> i32{
+    x + 1
+    // x + 1; これはエラーになる
 }
