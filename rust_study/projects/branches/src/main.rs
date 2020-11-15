cf60cb5fe1030e22864dacec7770eff454aab023
applego <applegorillappa@gmail.com>
2020-11-15 15:09:59 +0900

    if による条件分岐 let number = if condition {   5   } else {     6     };     という使用方法も

--- rust_study/projects/branches/src/main.rs
+++ rust_study/projects/branches/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 3;
+    is_greater_than_five(number);
+    let number = 7;
+    is_greater_than_five(number);
+
+    let condition = true;
+    five_or_six(condition);
+    assert_eq!(five_or_six(condition),5);
+    let condition = false;
+    assert_eq!(five_or_six(condition),6);
+}
+
+fn is_greater_than_five(number:i32) -> bool {
+    if number < 5 {
+        println!("condition was true");
+        return true;
+    } else {
+        println!("condition was false");
+        return false;
+    }
+}
+
+fn five_or_six(is_five:bool)->i32{
+    let number = if is_five {
+        5
+    } else{
+        6
+    };
+
+    println!("The value of number is: {}",number);
+    return number;
 }
---
+++ rust_study/projects/branches/target/rls/debug/deps/branches-24f34cea87133b8f.d
@@ -0,0 +1,5 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/branches/target/rls/debug/deps/branches-24f34cea87133b8f.rmeta: src/main.rs
+
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/branches/target/rls/debug/deps/branches-24f34cea87133b8f.d: src/main.rs
+
+src/main.rs:
