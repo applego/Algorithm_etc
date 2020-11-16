23a6a50f1cbb5e8e8179e39fcdf0aeca3f6ce4bc
applego <applegorillappa@gmail.com>
2020-11-17 00:25:26 +0900

    所有権と関数

--- rust_study/projects/ownership/src/main.rs
+++ rust_study/projects/ownership/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
-}
+    let s = String::from("hello"); // sがスコープに入る
+
+    takes_ownership(s); // sの値が関数にムーブされ...
+                        // ... ここではもう有効ではない
+                        // println!("{}",s);
+
+    let x = 5;
+
+    makes_copy(x); // xも関数にムーブされるが、
+                   // i32はCopyなので、この後にxを使っても
+                   // 大丈夫
+} // ここでxがスコープを抜け、sもスコープを抜ける.ただしsの値はムーブされているので、何も特別なことは起こらない
+
+fn takes_ownership(some_string: String) {
+    // some_string がスコープに入る
+    println!("{}", some_string);
+} // ここでsome_stringがスコープを抜け、`drop`が呼ばれる。後ろ盾してたメモリが開放される。
+
+fn makes_copy(some_integer: i32) {
+    // some_integerがスコープに入る
+    println!("{}", some_integer);
+} // ここでsome_integerがスコープを抜ける。なにも特別なことはない。
