9a0d9e8ba468819cca384c4098ac636acdd6760a
applego <applegorillappa@gmail.com>
2020-11-17 00:13:42 +0900

    codewars rustでも

---
+++ rust_study/projects/codewars_rust/src/kyu8_keep_hydrated.rs
@@ -0,0 +1,6 @@
+pub fn litres(time: f64) -> i32 {
+    // let ans = time / 2.0;
+    // let ans = ans as i32;
+    // return ans;
+    ((time / 2.0) as i32)
+}
--- rust_study/projects/codewars_rust/target/rls/debug/deps/codewars_rust-90ca78cce472f07b.d
+++ rust_study/projects/codewars_rust/target/rls/debug/deps/codewars_rust-90ca78cce472f07b.d
@@ -1,6 +1,7 @@
-/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/codewars_rust/target/rls/debug/deps/codewars_rust-90ca78cce472f07b.rmeta: src/lib.rs src/kyu8_reversed_words.rs
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/codewars_rust/target/rls/debug/deps/codewars_rust-90ca78cce472f07b.rmeta: src/lib.rs src/kyu8_keep_hydrated.rs src/kyu8_reversed_words.rs
 
-/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/codewars_rust/target/rls/debug/deps/codewars_rust-90ca78cce472f07b.d: src/lib.rs src/kyu8_reversed_words.rs
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/codewars_rust/target/rls/debug/deps/codewars_rust-90ca78cce472f07b.d: src/lib.rs src/kyu8_keep_hydrated.rs src/kyu8_reversed_words.rs
 
 src/lib.rs:
+src/kyu8_keep_hydrated.rs:
 src/kyu8_reversed_words.rs:
