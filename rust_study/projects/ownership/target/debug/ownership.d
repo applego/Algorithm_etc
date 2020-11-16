d83cc7bc7bbcd6c66efb401d36e366e85cf1c887
applego <applegorillappa@gmail.com>
2020-11-17 00:17:06 +0900

    add project named ownership

---
+++ rust_study/projects/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/ownership/target/debug/ownership: /Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/ownership/src/main.rs
---
+++ rust_study/projects/ownership/target/rls/debug/deps/ownership-b4a43e1046150631.d
@@ -0,0 +1,5 @@
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/ownership/target/rls/debug/deps/ownership-b4a43e1046150631.rmeta: src/main.rs
+
+/Users/tokoshige_mac/Projects/Atcoder/rust_study/projects/ownership/target/rls/debug/deps/ownership-b4a43e1046150631.d: src/main.rs
+
+src/main.rs:
