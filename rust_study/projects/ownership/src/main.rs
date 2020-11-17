4f5ef3fd6ad3e6992e64599bedf41cdcffe66f7d
applego <applegorillappa@gmail.com>
2020-11-17 23:01:17 +0900

    参照と借用章　終わり

--- rust_study/projects/ownership/src/main.rs
+++ rust_study/projects/ownership/src/main.rs
@@ -11,6 +11,18 @@ fn main() {
     でも、これでは、大袈裟すぎますし、ありふれているはずの概念に対して、作業量が多すぎます。
     私たちにとって幸運なことに、Rustにはこの概念に対する機能があり、参照と呼ばれます。
      */
+
+    // 参照と借用
+    references_and_borrow();
+    /*
+    これらのアンド記号が参照であり、
+    これのおかげで所有権をもらうことなく値を参照することができるのです。 図4-5はその図解です。
+     */
+
+    borrow_de_nanika_wo_henkousiyoutositara();
+
+    //ダングリング参照を阻止
+    let reference_to_nothing = dangle();
 }
 
 fn shoyuuken_to_kannsuu() {
@@ -79,4 +91,64 @@ fn calculate_length(s: String) -> (String, usize) {
     let length = s.len(); // len()メソッドは、Stringの長さを返します
 
     (s, length)
+}
+
+fn references_and_borrow() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length_ref(&s1);
+
+    // '{}'の長さは、{}です
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length_ref(s: &String) -> usize {
+    // sはStringへの参照
+    s.len()
+} // ここで、sはスコープ外になる。けど、参照しているものの所有権を持っているわけではないので
+  //  何も起こらない
+
+fn borrow_de_nanika_wo_henkousiyoutositara() {
+    let s = String::from("hello");
+
+    change(&s);
+
+    // (エラー: 一度に`s`を可変として2回以上借用することはできません) のはずだがコンパイル通るっぽい?
+    let mut s2 = String::from("hello");
+
+    let r1 = &mut s2;
+    let r2 = &mut s2;
+
+    let mut s3 = String::from("hello");
+    {
+        let r3 = &mut s3;
+    } // r3はここでスコープを抜けるので、問題なく新しい参照を作ることができる
+    let r4 = &mut s3;
+
+    let mut s4 = String::from("hello4");
+
+    let r5 = &s4; // 問題なし
+    let r6 = &s4; // 問題なし
+    let r7 = &mut s4; // 大問題!
+
+    println!("{}", r7);
+}
+
+fn change(some_string: &String) {
+    // some_string.push_str(", world");
+    /*
+        cannot borrow immutable borrowed content `*some_string` as mutable
+    (エラー: 不変な借用をした中身`*some_string`を可変で借用できません)
+         */
+}
+
+fn dangle() -> String {
+    //解決策 ↑
+
+    //&String {
+    // expected named lifetime parameter
+    let s = String::from("hello");
+
+    // &s
+    s // 解決策
 }
