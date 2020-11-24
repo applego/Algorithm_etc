5c6b2e57c6846b4db684a2719958411c7c51d36a
applego <applegorillappa@gmail.com>
2020-11-24 22:43:45 +0900

    .ドットでアクセス

--- rust_study/projects/struct_study/src/main.rs
+++ rust_study/projects/struct_study/src/main.rs
@@ -6,10 +6,12 @@ struct User {
 }
 
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
     };
+
+    user1.email = String::from("anotheremail@example.com");
 }
