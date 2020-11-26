919edbc3a541b715653c81756c174ef59da36bba
applego <applegorillappa@gmail.com>
2020-11-26 21:12:31 +0900

    関連関数 implブロックの別の有益な機能は、implブロック内にselfを引数に取らない関数を定義できることです。 これは、構造体に関連付けられているので、関連関数と呼ばれます。それでも、関連関数は関数であり、メソッドではありません。 というのも、対象となる構造体のインスタンスが存在しないからです。もうString::fromという関連関数を使用したことがありますね。
    関連関数は、構造体の新規インスタンスを返すコンストラクタによく使用されます。例えば、一つの寸法を引数に取り、 長さと幅両方に使用する関連関数を提供することができ、その結果、同じ値を2回指定する必要なく、 正方形のRectangleを生成しやすくすることができます。

--- rust_study/projects/rectangles/src/main.rs
+++ rust_study/projects/rectangles/src/main.rs
@@ -8,8 +8,16 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
     }
 }
 
@@ -60,6 +68,10 @@ fn main() {
     //rect1にrect2ははまり込む?
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    // 関連関数
+    let square1 = Rectangle::square(3);
+    println!("square1 is {:?}", square1);
 }
 
 // fn area(rectangle: &Rectangle) -> u32 {
