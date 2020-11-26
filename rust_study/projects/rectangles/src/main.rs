c33cdedc9ba513fba047d808be02dfde972293d9
applego <applegorillappa@gmail.com>
2020-11-26 20:54:38 +0900

    構造体でリファクタリングする: より意味付けする

--- rust_study/projects/rectangles/src/main.rs
+++ rust_study/projects/rectangles/src/main.rs
@@ -1,15 +1,28 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     // let width1 = 30;
     // let height1 = 50;
-    let rect1 = (30, 50);
+
+    // let rect1 = (30, 50);
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         // 長方形の面積は、｛｝平方ピクセルです
         "The area of rectangle is {} aquare pixels.",
-        area(rect1)
+        area(&rect1)
     );
+
+    println!("rect1.width: {}", rect1.width);
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
