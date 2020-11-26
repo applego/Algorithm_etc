75b72c7b7d54bb666cc78a2356182113211520ef
applego <applegorillappa@gmail.com>
2020-11-26 20:59:54 +0900

    トレイトの導出で有用な機能を追加する   プログラムのデバッグをしている間に、Rectangleのインスタンスを出力し、フィールドの値を確認できると、 素晴らしいわけです。リスト5-11では、以前の章のように、println!マクロを試しに使用しようとしていますが、動きません。

--- rust_study/projects/rectangles/src/main.rs
+++ rust_study/projects/rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -13,6 +14,18 @@ fn main() {
         width: 30,
         height: 50,
     };
+
+    // println!("rect1 is {}", rect1);
+    // `Rectangle` doesn't implement `std::fmt::Display`
+    // `Rectangle` cannot be formatted with the default formatter
+
+    // println!("rect1 is {:?}", rect1);
+    // `Rectangle` doesn't implement `std::fmt::Debug`
+    // `Rectangle` cannot be formatted using `{:?}`
+
+    // #[derive(Debug)]を先頭に付ける →　OK
+
+    println!("rect1 is {:?}", rect1);
 
     println!(
         // 長方形の面積は、｛｝平方ピクセルです
