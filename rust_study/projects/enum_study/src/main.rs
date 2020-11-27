93921dd0b4b73f70c91f9fbf413f654f32d7a089
applego <applegorillappa@gmail.com>
2020-11-27 11:31:01 +0900

    リスト6-1: IPアドレスのデータとIpAddrKindの列挙子をstructを使って保持する

--- rust_study/projects/enum_study/src/main.rs
+++ rust_study/projects/enum_study/src/main.rs
@@ -3,12 +3,22 @@ enum IpAddrKind {
     V6,
 }
 
-fn route(ip_type: IpAddrKind) {}
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
 
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
-    route(four);
-    route(six);
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
 }
