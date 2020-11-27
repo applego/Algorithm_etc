dd73c65b50787ad037393e2a860d626c31b92149
applego <applegorillappa@gmail.com>
2020-11-27 11:33:51 +0900

    enumの各列挙子にデータを直接添付できるので、余計な構造体を作る必要は全くありません。

--- rust_study/projects/enum_study/src/main.rs
+++ rust_study/projects/enum_study/src/main.rs
@@ -3,22 +3,32 @@ enum IpAddrKind {
     V6,
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    // structを使用
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    // enumで
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
 }
