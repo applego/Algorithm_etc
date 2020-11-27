7316a17e027cbf7ae545dc5c48557e21da21b316
applego <applegorillappa@gmail.com>
2020-11-27 11:42:31 +0900

    enum 幅広い種類の型が列挙子に埋め込まれている enumにもメソッドを定義できる

--- rust_study/projects/enum_study/src/main.rs
+++ rust_study/projects/enum_study/src/main.rs
@@ -35,6 +35,9 @@ fn main() {
     // enumで（V4とV6で型を変える ver.
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
 
 // 標準ライブラリ
@@ -45,4 +48,18 @@ struct Ipv6Addr {}
 enum IpAddr_lib {
     V4(Ipv4Addr),
     V6(Ipv6Addr),
+}
+
+// enum 幅広い種類の型が列挙子に埋め込まれている
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
 }
