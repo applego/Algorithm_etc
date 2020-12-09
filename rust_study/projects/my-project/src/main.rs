0488da2b2180f77d49a3c495f1a6f4353496b72f
applego <applegorillappa@gmail.com>
2020-12-09 23:40:02 +0900

    // リスト8-8: forループで要素を走査し、ベクタの要素を出力する // リスト8-9: ベクタの要素への可変な参照を走査する

--- rust_study/projects/my-project/src/main.rs
+++ rust_study/projects/my-project/src/main.rs
@@ -23,13 +23,36 @@ fn main() {
     let third: Option<&i32> = v.get(2);
 
     // リスト8-6: 5つの要素を含むベクタの添え字100の要素にアクセスしようとする
-    let does_not_exist = &v[100];
+    // let does_not_exist = &v[100];
     let does_not_exist = v.get(100);
 
     //同一スコープ上では、
     //可変と不変な参照を同時には存在させられないというルールを思い出してください。
-    let mut v = vec![1, 2, 3, 4, 5];
-    let first = &v[0];
-    v.push(6);
-    println!("The first element is: {}", first);
+
+    // error[E0502]: cannot borrow `v` as mutable because it is also borrowed as immutable
+    //   --> src/main.rs:33:5
+    //    |
+    // 32 |     let first = &v[0];
+    //    |                  - immutable borrow occurs here
+    // 33 |     v.push(6);
+    //    |     ^^^^^^^^^ mutable borrow occurs here
+    // 34 |     println!("The first element is: {}", first);
+    //    |                                          ----- immutable borrow later used here
+
+    // let mut v = vec![1, 2, 3, 4, 5];
+    // let first = &v[0];
+    // v.push(6);
+    // println!("The first element is: {}", first);
+
+    // リスト8-8: forループで要素を走査し、ベクタの要素を出力する
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    // リスト8-9: ベクタの要素への可変な参照を走査する
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
 }
