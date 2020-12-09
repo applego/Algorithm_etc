68aa31e5958c1d8e370a98d14b551b801193683e
applego <applegorillappa@gmail.com>
2020-12-09 23:34:28 +0900

    同一スコープ上では、可変と不変な参照を同時には存在させられないというルールを思い出してください。
    リスト8-7のコードは、一見動くはずのように見えるかもしれません: なぜ、最初の要素への参照が、 ベクタの終端への変更を気にかける必要があるのでしょうか？このエラーは、ベクタの動作法のせいです: 新規要素をベクタの終端に追加すると、ベクタが現在存在する位置に隣り合って要素を入れるだけの領域がなかった場合に、 メモリの新規確保をして古い要素を新しいスペースにコピーする必要があるかもしれないからです。 その場合、最初の要素を指す参照は、解放されたメモリを指すことになるでしょう。借用規則により、 そのような場面に陥らないよう回避されるのです。

--- rust_study/projects/my-project/src/main.rs
+++ rust_study/projects/my-project/src/main.rs
@@ -16,4 +16,20 @@ fn main() {
     v.push(7);
     v.push(8);
     println!("{:?}", v);
+
+    // リスト8-5: 添字記法かgetメソッドを使用してベクタの要素にアクセスする
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    let third: Option<&i32> = v.get(2);
+
+    // リスト8-6: 5つの要素を含むベクタの添え字100の要素にアクセスしようとする
+    let does_not_exist = &v[100];
+    let does_not_exist = v.get(100);
+
+    //同一スコープ上では、
+    //可変と不変な参照を同時には存在させられないというルールを思い出してください。
+    let mut v = vec![1, 2, 3, 4, 5];
+    let first = &v[0];
+    v.push(6);
+    println!("The first element is: {}", first);
 }
