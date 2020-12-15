53bf5bdadc89fe1acd52238d7eb087c1ed558b56
applego <applegorillappa@gmail.com>
2020-12-15 18:09:31 +0900

    久々Rustクリア

--- codewars/rust/codewars_sotuions/src/solutions/eight_reversed_strings.rs
+++ codewars/rust/codewars_sotuions/src/solutions/eight_reversed_strings.rs
@@ -1,3 +1,11 @@
+fn solution_draft(phrase: &str) -> String {
+    let phase1 = phrase.chars().rev();
+    println!("{:?}",phase1);
+    let phase2 = phase1.collect();
+    return phase2;
+    // unimplemented!();
+}
+
 pub fn solution(phrase: &str) -> String {
-    unimplemented!();
+    return phrase.chars().rev().collect();
 }
