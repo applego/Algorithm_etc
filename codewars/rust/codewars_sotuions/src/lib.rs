456772bf353c2de0c48d45bb49e353814e6cb656
applego <applegorillappa@gmail.com>
2020-12-16 19:20:43 +0900

    Rust &strをi32に parse().unwrap() parse::<i32>().unwrap_or(0)

--- codewars/rust/codewars_sotuions/src/lib.rs
+++ codewars/rust/codewars_sotuions/src/lib.rs
@@ -13,4 +13,14 @@ mod tests {
     fn sample_test() {
         assert_eq!(eight_reversed_strings::solution("world"), "dlrow");
     }
+
+    // 2020/12/16 19:13
+    use crate::solutions::eight_convert_a_string_to_a_number::string_to_number;
+    #[test]
+    fn returns_expected() {
+      assert_eq!(string_to_number("1234"), 1234);
+      assert_eq!(string_to_number("605"), 605);
+      assert_eq!(string_to_number("1405"), 1405);
+      assert_eq!(string_to_number("-7"), -7);
+    }
 }
--- codewars/rust/codewars_sotuions/src/solutions.rs
+++ codewars/rust/codewars_sotuions/src/solutions.rs
@@ -1,1 +1,2 @@
 pub mod eight_reversed_strings;
+pub mod eight_convert_a_string_to_a_number;
