b8630ce746e3763fe867d4f62900ca5ba7ec8411
applego <applegorillappa@gmail.com>
2020-12-22 21:59:30 +0900

    1-1. 正解

--- templates/Typescript_型.js
+++ templates/Typescript_型.js
@@ -2,7 +2,6 @@
 https://qiita.com/uhyo/items/e4f54ef3b87afdd65546?utm_content=buffer525a1&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer&fbclid=IwAR0x2JkKQDEtSLd-qEoaBiL7TfetyMHHEX0Q7fjt7YN9_XtmMvDw29Qoegk
 
 */
-
 /**
 難易度：★☆☆☆　基礎の基礎レベル
 基本的な型が分かっていれば解ける問題たちです。
@@ -12,14 +11,11 @@ https://qiita.com/uhyo/items/e4f54ef3b87afdd65546?utm_content=buffer525a1&utm_me
 この関数に正しく型アノテーションを付けてください。
 型アノテーションとは、引数や返り値の型をソースコード中に明示することです。
  */
-
-function isPositive(num:number) :boolean {
+function isPositive(num) {
     return num >= 0;
 }
-
 // 使用例
 isPositive(3);
-
 // エラー例
 // isPositive('123');
 // const numVar: number = isPositive(-5);
