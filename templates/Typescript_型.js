394a4cc9792ed340c0c8aeab3d48cf0880e69d0f
applego <applegorillappa@gmail.com>
2020-12-22 22:19:17 +0900

    1-4. 解答

--- templates/Typescript_型.js
+++ templates/Typescript_型.js
@@ -31,3 +31,13 @@ isPositive13(5);
 // エラー例
 // isPositive13('foo');
 // const res: number = isPositive13(123);
+// 1-4. 配列の型
+// 以下のコードで定義される関数sumOfPosは、数値の配列を受け取って、そのうち0以上の値の和を返す関数です。適切な型アノテーションをつけてください。
+function sumOfPos(arr /* :Array<number> */) {
+    return arr.filter(function (num) { return num >= 0; }).reduce(function (acc, num) { return acc + num; }, 0);
+}
+// 使用例
+var sum = sumOfPos([1, 3, -2, 0]);
+// エラー例
+// sumOfPos(123, 456);
+// sumOfPos([123, "foobar"]);
--- templates/Typescript_型.ts
+++ templates/Typescript_型.ts
@@ -87,7 +87,7 @@ isPositive13(5)
 
 // 1-4. 配列の型
 // 以下のコードで定義される関数sumOfPosは、数値の配列を受け取って、そのうち0以上の値の和を返す関数です。適切な型アノテーションをつけてください。
-function sumOfPos(arr) {
+function sumOfPos(arr: number[]/* :Array<number> */):number {
     return arr.filter(num => num >= 0).reduce((acc, num) => acc + num, 0);
 }
 
@@ -95,5 +95,5 @@ function sumOfPos(arr) {
 const sum: number = sumOfPos([1, 3, -2, 0]);
 
 // エラー例
-sumOfPos(123, 456);
-sumOfPos([123, "foobar"]);
+// sumOfPos(123, 456);
+// sumOfPos([123, "foobar"]);
