0e7392a8a059691f233997195f1d14cdcda1a343
applego <applegorillappa@gmail.com>
2020-12-22 22:43:23 +0900

    2-1. 解答　ジェネリクスわかってきたぞ

--- templates/型/Typescript_型2.js
+++ templates/型/Typescript_型2.js
@@ -2,26 +2,23 @@
 // https://qiita.com/uhyo/items/e4f54ef3b87afdd65546?utm_content=buffer525a1&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer&fbclid=IwAR0x2JkKQDEtSLd-qEoaBiL7TfetyMHHEX0Q7fjt7YN9_XtmMvDw29Qoegk
 // 難易度：★★☆☆　基本レベル
 // よく使う機能を一通り知っていれば解ける問題たちです。
-
 // 2-1. ジェネリクス
 // 以下のコードで定義される関数myFilterは、配列のfilter関数を再実装したものです。myFilter関数に適切な型アノテーションを付けてください。
 // myFilter関数は色々な型の配列を受け取れる点に注意してください。必要に応じてmyFilterに型引数を追加しても構いません。
-
-function myFilter<T>(arr: Array<T>, predicate: (elm: T) => boolean): Array<T>{
-    const result = [];
-    for (const elm of arr) {
+function myFilter(arr, predicate) {
+    var result = [];
+    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+        var elm = arr_1[_i];
         if (predicate(elm)) {
             result.push(elm);
         }
     }
     return result;
 }
-
 // 使用例
-const res = myFilter([1, 2, 3, 4, 5], num => num % 2 === 0);
-const res2 = myFilter(['foo', 'hoge', 'bar'], str => str.length >= 4);
+var res = myFilter([1, 2, 3, 4, 5], function (num) { return num % 2 === 0; });
+var res2 = myFilter(['foo', 'hoge', 'bar'], function (str) { return str.length >= 4; });
 console.log(res);
 console.log(res2);
-
 // エラー例
 // myFilter([1, 2, 3, 4, 5], str => str.length >= 4);
