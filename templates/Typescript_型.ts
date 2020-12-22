27f109aafe9a3377329e8df19781954d3b75baf1
applego <applegorillappa@gmail.com>
2020-12-22 22:16:30 +0900

    1-4. 配列の型

--- templates/Typescript_型.ts
+++ templates/Typescript_型.ts
@@ -84,3 +84,16 @@ isPositive13(5)
 // エラー例
 // isPositive13('foo');
 // const res: number = isPositive13(123);
+
+// 1-4. 配列の型
+// 以下のコードで定義される関数sumOfPosは、数値の配列を受け取って、そのうち0以上の値の和を返す関数です。適切な型アノテーションをつけてください。
+function sumOfPos(arr) {
+    return arr.filter(num => num >= 0).reduce((acc, num) => acc + num, 0);
+}
+
+// 使用例
+const sum: number = sumOfPos([1, 3, -2, 0]);
+
+// エラー例
+sumOfPos(123, 456);
+sumOfPos([123, "foobar"]);
