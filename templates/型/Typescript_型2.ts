5ccdb34f91b8cf796e2c29b8524b6d9ef198b032
applego <applegorillappa@gmail.com>
2020-12-22 22:58:17 +0900

    2-3 省略可能なプロパティ

--- templates/型/Typescript_型2.ts
+++ templates/型/Typescript_型2.ts
@@ -60,3 +60,30 @@ const fastSpeed = getSpeed("fast");
 
 // エラー例
 // getSpeed("veryfast");
+
+/*
+2-3. 省略可能なプロパティ
+EventTarget#addEventListenerは、2つまたは3つの引数を受け取る関数で、返り値はありません。1つ目の引数は文字列、2つ目の引数は関数です。そして3つ目の引数は省略可能であり、真偽値またはオブジェクトを渡すことができます。オブジェクトに存在可能なプロパティはcapture, once, passiveの3つで、全て真偽値であり、省略可能です。
+このようなインターフェースを持つ関数addEventListenerをdeclareを用いて宣言してください。簡単のために、第2引数に指定する関数は引数無しで何も返さない関数としてください。
+*/
+
+// なお、declareはTypeScriptに特有の構文であり、以下のように関数や変数の型を中身なしに宣言できる構文です。
+// declareの例
+// declare function foo(arg: number): number;
+
+// 使用例
+addEventListener("foobar", () => { });
+addEventListener("event", () => { }, true);
+addEventListener("event2", () => { }, {});
+addEventListener("event3", () => { }, {
+    capture: true,
+    once: false
+});
+
+// エラー例
+addEventListener("foobar", () => { }, "string");
+addEventListener("hoge", () => { }, {
+    capture: true,
+    once: false,
+    excess: true
+});
