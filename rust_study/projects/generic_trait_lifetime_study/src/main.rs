159bb84cc72b0770b788a67ce2250b0347067677
applego <applegorillappa@gmail.com>
2020-12-24 22:24:58 +0900

    リスト10-5: ジェネリックな型引数を使用するものの、まだコンパイルできないlargest関数の定義

--- rust_study/projects/generic_trait_lifetime_study/src/main.rs
+++ rust_study/projects/generic_trait_lifetime_study/src/main.rs
@@ -29,6 +29,17 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -38,5 +49,13 @@ fn main() {
     let char_list = vec!['y', 'm', 'a', 'q'];
 
     let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
