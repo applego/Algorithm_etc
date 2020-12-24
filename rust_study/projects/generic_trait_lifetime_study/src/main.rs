5adde10e904d47891faee1b5c78935146ddc70e9
applego <applegorillappa@gmail.com>
2020-12-24 22:29:52 +0900

    リスト10-6: 型Tのxとy値を保持するPoint<T>構造体 リスト10-8: Point<T, U>は2つの型に関してジェネリックなので、xとyは異なる型の値になり得る

--- rust_study/projects/generic_trait_lifetime_study/src/main.rs
+++ rust_study/projects/generic_trait_lifetime_study/src/main.rs
@@ -40,22 +40,39 @@ fn largest<T>(list: &[T]) -> T {
     largest
 }
 
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest_i32(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+struct Point<T> {
+    x: T,
+    y: T,
 }
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    let wont_work = Point2 { x: 5, y: 4.0 }; //* Tは同じ型の必要がある
+}
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest_i32(&number_list);
+//     println!("The largest number is {}", result);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest_char(&char_list);
+//     println!("The largest char is {}", result);
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest(&char_list);
+//     println!("The largest char is {}", result);
+// }
