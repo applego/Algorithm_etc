e2d5c397f85eabd2fca764298faf2bd67c8bf7af
applego <applegorillappa@gmail.com>
2020-12-27 23:39:38 +0900

    リスト11-3: panic!マクロを呼び出したために失敗する2番目のテストを追加する

--- rust_study/projects/adder/src/lib.rs
+++ rust_study/projects/adder/src/lib.rs
@@ -1,7 +1,13 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn exploration() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        // 失敗させる
+        panic!("Make this test fail");
     }
 }
