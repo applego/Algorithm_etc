610758159e4c165eea15d627a346be54242543d6
applego <applegorillappa@gmail.com>
2021-01-05 00:00:40 +0900

    Timeout突破できず setをうまく使うとクリア

--- build/js/codewars/main.js
+++ build/js/codewars/main.js
@@ -7,6 +7,19 @@ var TestCodewar_1 = __importDefault(require("./TestCodewar"));
 // let test = new Test();//__filename
 var TimeMeasure_1 = __importDefault(require("./TimeMeasure"));
 var chai_1 = require("chai");
+// 2021/01/04 21:06
+//  上から書いていくスタイルにチェンジ
+var _5_kyu_Sum_of_pairs_1 = require("./5 kyu Sum of pairs");
+var l1 = [1, 4, 8, 7, 3, 15], l2 = [1, -2, 3, 0, -6, 1], l3 = [20, -13, 40], l4 = [1, 2, 3, 4, 1, 0], l5 = [10, 5, 2, 3, 7, 5], l6 = [4, -2, 3, 3, 4], l7 = [0, 2, 0], l8 = [5, 9, 13, -3];
+chai_1.assert.deepEqual(_5_kyu_Sum_of_pairs_1.sumPairs(l1, 8), [1, 7], "Basic: [" + l1 + "] should return [1, 7] for sum = 8");
+chai_1.assert.deepEqual(_5_kyu_Sum_of_pairs_1.sumPairs(l2, -6), [0, -6], "Negatives: [" + l2 + "] should return [0, -6] for sum = -6");
+chai_1.assert.deepEqual(_5_kyu_Sum_of_pairs_1.sumPairs(l3, -7), undefined, "No Match: [" + l3 + "] should return undefined for sum = -7");
+chai_1.assert.deepEqual(_5_kyu_Sum_of_pairs_1.sumPairs(l4, 2), [1, 1], "First Match From Left: [" + l4 + "] should return [1, 1] for sum = 2 ");
+chai_1.assert.deepEqual(_5_kyu_Sum_of_pairs_1.sumPairs_BP(l5, 10), [3, 7], "First Match From Left REDUX!: [" + l5 + "] should return [3, 7] for sum = 10 ");
+chai_1.assert.deepEqual(_5_kyu_Sum_of_pairs_1.sumPairs(l5, 10), [3, 7], "First Match From Left REDUX!: [" + l5 + "] should return [3, 7] for sum = 10 ");
+chai_1.assert.deepEqual(_5_kyu_Sum_of_pairs_1.sumPairs(l6, 8), [4, 4], "Duplicates: [" + l6 + "] should return [4, 4] for sum = 8");
+chai_1.assert.deepEqual(_5_kyu_Sum_of_pairs_1.sumPairs(l7, 0), [0, 0], "Zeroes: [" + l7 + "] should return [0, 0] for sum = 0");
+chai_1.assert.deepEqual(_5_kyu_Sum_of_pairs_1.sumPairs(l8, 10), [13, -3], "Subtraction: [" + l8 + "] should return [13, -3] for sum = 10");
 var speedOfPetals = require("./8 kyu The falling speed of petals");
 // speedOfPetals.sakuraFall(0);
 // speedOfPetals.sakuraFall(5);
--- codewars/main.ts
+++ codewars/main.ts
@@ -6,6 +6,27 @@ import TimeMeasure from './TimeMeasure';
 import { describe, it } from "mocha";
 import { assert, expect } from "chai";
 
+// 2021/01/04 21:06
+//  上から書いていくスタイルにチェンジ
+import { sumPairs, sumPairs_BP } from "./5 kyu Sum of pairs";
+const l1: number[] = [1, 4, 8, 7, 3, 15],
+l2: number[] = [1, -2, 3, 0, -6, 1],
+l3: number[] = [20, -13, 40],
+l4: number[] = [1, 2, 3, 4, 1, 0],
+l5: number[] = [10, 5, 2, 3, 7, 5],
+l6: number[] = [4, -2, 3, 3, 4],
+l7: number[] = [0, 2, 0],
+l8: number[] = [5, 9, 13, -3];
+assert.deepEqual(sumPairs(l1, 8), [1, 7], "Basic: ["+l1+"] should return [1, 7] for sum = 8");
+assert.deepEqual(sumPairs(l2, -6), [0, -6], "Negatives: ["+l2+"] should return [0, -6] for sum = -6");
+assert.deepEqual(sumPairs(l3, -7), undefined, "No Match: ["+l3+"] should return undefined for sum = -7");
+assert.deepEqual(sumPairs(l4, 2), [1, 1], "First Match From Left: ["+l4+"] should return [1, 1] for sum = 2 ");
+assert.deepEqual(sumPairs_BP(l5, 10), [3, 7], "First Match From Left REDUX!: ["+l5+"] should return [3, 7] for sum = 10 ");
+assert.deepEqual(sumPairs(l5, 10), [3, 7], "First Match From Left REDUX!: [" + l5 + "] should return [3, 7] for sum = 10 ");
+assert.deepEqual(sumPairs(l6, 8), [4, 4], "Duplicates: ["+l6+"] should return [4, 4] for sum = 8");
+assert.deepEqual(sumPairs(l7, 0), [0, 0], "Zeroes: ["+l7+"] should return [0, 0] for sum = 0");
+assert.deepEqual(sumPairs(l8, 10), [13, -3], "Subtraction: ["+l8+"] should return [13, -3] for sum = 10");
+
 import speedOfPetals = require("./8 kyu The falling speed of petals");
 // speedOfPetals.sakuraFall(0);
 // speedOfPetals.sakuraFall(5);
