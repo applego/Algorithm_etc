9d1467fbcbd52a0718d1bf0911efddd1339853a5
applego <applegorillappa@gmail.com>
2021-01-31 02:58:31 +0900

    add BP

--- codewars/5 kyu Product of consecutive Fib numbers.ts
+++ codewars/5 kyu Product of consecutive Fib numbers.ts
@@ -1,3 +1,13 @@
+
+// BP
+export class G964_20210130_BP{
+  public static productFib = (prod: number) => {
+    let p1: number = 1, p2: number = 1;
+    while (p1 * p2 < prod) [p1, p2] = [p2, p1 + p2];
+    return [p1, p2, (p1 * p2 == prod)];
+  }
+}
+
 interface Dictionary{
   [key: number]: number;
 }
