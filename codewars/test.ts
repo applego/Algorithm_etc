66be19a6890cdc7957d81c20fc281249c0b75df7
applego <applegorillappa@gmail.com>
2021-03-08 22:27:24 +0900

    easy

--- codewars/test.ts
+++ codewars/test.ts
@@ -16,3 +16,35 @@ describe('../AtCoder/abc002B - 罠.ts', () => {
     assert.deepEqual(a20210305_2('mazushii'), 'mzsh');
   });
 });
+
+// 2021/03/05 22:26
+import { findSmallestInt } from './8 kyu Find the smallest integer in the array';
+describe('Smallest Integer Tests', function () {
+  it('Fixed Tests', function () {
+    assert.strictEqual(
+      findSmallestInt([78, 56, 232, 12, 8]),
+      8,
+      'Should return the smallest int 8'
+    );
+    assert.strictEqual(
+      findSmallestInt([78, 56, 232, 12, 18]),
+      12,
+      'Should return the smallest int 12'
+    );
+    assert.strictEqual(
+      findSmallestInt([78, 56, 232, 412, 228]),
+      56,
+      'Should return the smallest int 56'
+    );
+    assert.strictEqual(
+      findSmallestInt([78, 56, 232, 12, 0]),
+      0,
+      'Should return the smallest int 0'
+    );
+    assert.strictEqual(
+      findSmallestInt([1, 56, 232, 12, 8]),
+      1,
+      'Should return the smallest int 1'
+    );
+  });
+});
