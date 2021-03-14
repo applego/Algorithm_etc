757c29928e181f84183d6dce91fcfd9dd2136c84
applego <applegorillappa@gmail.com>
2021-03-15 00:15:32 +0900

    go test できた(main.go）

--- codewars/main.go
+++ codewars/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+// 同一package内なら小文字始まりのstructでOKだが、別packageの場合大文字で
+type mydata struct {
+	num int
+	str string
+}
+
 func main() {
-	fmt.Printf("main.go main()")
+	var x mydata
+	x.num = 11
+	x.str = "apple"
+	fmt.Printf("%#v\n",x)
+
+	result := Calculate(3)
+	fmt.Printf("3+2=%v\n",result)
+}
+
+func Calculate(x int)(result int){
+	result = x+2
+	return result
 }
---
+++ codewars/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+// Simple test code
+func TestCaluculate(t *testing.T){
+	output := Calculate(2)
+	if output != 4{
+		t.Errorf("Caluculate(2) = %v, expected 4", output)
+	}
+}
+
+// テーブル駆動テストの手法に沿ったコード
+func TestTableCaluculate(t *testing.T){// *はポインタ型の宣言を使用するときに使用する(&は変数toポインタ、*はポインタto変数)
+	var tests = []struct {
+		input int
+		expected int
+	}{
+		{2,4},
+		{-1,1},
+		{0,2},
+		{-5,-3},
+		{999,1001},
+	}
+
+	for _,test := range tests{
+		output := Calculate(test.input)
+		if output != test.expected{
+			t.Errorf("Test Failed: input %v, got %v, want %v",test.input,output,test.expected)
+		}
+	}
+}
