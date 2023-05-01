0b896ff500004c4ccd6fc89c1238eec8d301fdd4
applego <applegorillappa@gmail.com>
2023-05-01 14:01:50 +0900

    素因数分解学習ルートでOK

---
+++ AtCoder/code.cpp
@@ -0,0 +1,74 @@
+// ここは競プロではサボりがちです
+#include <bits/stdc++.h>
+using namespace std;
+
+// デバッグ用マクロです。詳しくは https://naskya.net/post/0002/ 
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+// 節操ないですが、競プロでは便利です。
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long long>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vb = vector<bool>;
+using vpii = vector<pair<int, int>>;
+using vpll = vector<pair<long long, long long>>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<long long>>;
+using vvc = vector<vector<char>>;
+using vvb = vector<vector<bool>>;
+using vvvi = vector<vector<vector<int>>>;
+using pii = pair<int, int>;
+
+// ACLです。使わない時はコメントアウトしています。導入方法はググってみてください。
+// #include <atcoder/all>
+// using namespace atcoder;
+
+// 競プロerはrepマクロが大好き
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+
+// 無くても困らない
+#define INFTY (1 << 30)
+
+// 浮動小数点の誤差を考慮した等式ですが、使わずに済むなら使わない方が確実です
+#define EPS (1e-7)
+#define equal(a, b) (fabs((a) - (b)) < EPS)
+
+// DPやlong longの最大値最小値更新で重宝します。
+template <typename T>
+inline bool chmax(T &a, T b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T>
+inline bool chmin(T &a, T b) {
+  return ((a > b) ? (a = b, true) : (false));
+}
+
+
+// 手元で複数ケースを同時に試したい時に、tsの値を増やして適当なファイルからリダイレクトするだけで済むので便利です
+// ※解説放送をしているすぬけさんのパクリ
+struct Solver {
+  void solve() {
+    /* input */
+
+    /* solve */
+
+    /* output */
+  }
+};
+
+int main() {
+  int ts = 1;
+  rep(ti, ts) {
+    Solver solver;
+    solver.solve();
+  }
+  return 0;
+}
---
+++ templates/約数判定.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<long long> enum_divisors(long long N) {
+    vector<long long> res;
+    for (long long i = 1; i * i <= N; ++i) {
+        if (N % i == 0) {
+            res.push_back(i);
+            // 重複しないならば i の相方である N/i も push
+            if (N/i != i) res.push_back(N/i);
+        }
+    }
+    // 小さい順に並び替える
+    sort(res.begin(), res.end());
+    return res;
+}
+
+int main() {
+    long long N;
+    cin >> N;
+    const auto &res = enum_divisors(N);
+    for (int i = 0; i < res.size(); ++i) cout << res[i] << " ";
+    cout << endl;
+}
